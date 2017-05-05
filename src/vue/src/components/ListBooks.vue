986486fa1b4359e04f01284c7129f064b59e5208
Mateus Alcova <mateusalcova@gmail.com>
2017-05-04 22:18:43 -0300

    Added new components in Vue.js

---
+++ src/vue/src/components/ListBooks.vue
@@ -0,0 +1,40 @@
+
+
+<template lang="jade">
+
+    .list-books
+
+        .title-list
+            span Listando 20 de 40 livros.
+
+        .gridbooks
+            book-box
+
+
+        .pages-box
+            a.btn-main.-disabled Anterior
+            .pages
+                input( type = 'text' name = 'page' value = '1' )
+                span de 2
+
+            a.btn-main Próximo
+
+
+
+</template>
+
+
+<script>
+
+    import BookBox from './BookBox.vue'
+
+    export default {
+
+        components: {
+            'book-box': BookBox
+        }
+
+    }
+
+
+</script>
--- src/vue/src/main.js
+++ src/vue/src/main.js
@@ -2,10 +2,13 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import Login from './components/Login.vue'
+import FilterBooks from './components/FilterBooks.vue'
+import ListBooks from './components/ListBooks.vue'
 
 Vue.use(VueResource)
-
-Vue.component('Login', Login)  
+Vue.component('Login', Login)
+Vue.component('FilterBooks', FilterBooks)
+Vue.component('ListBooks', ListBooks)
 
 new Vue({
     el: '.container'
